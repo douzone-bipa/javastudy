f07e6e4db295fed3adf24d54ef37f95917cb4f7d
더존-부산정보산업진흥원 <90119630+douzone-bipa@users.noreply.github.com>
2021-09-15 17:27:09 +0900

    Update NSLookup.java

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,20 +2,34 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.douzone.com";
-		
-		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName(line);
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+				
+				if("exit".equals(line)) {
+					break;
+				}
+				
+				InetAddress[] inetAddresses = 
+						InetAddress.getAllByName(line);
+				
+				for(InetAddress inetAddress : inetAddresses) {
+					System.out.println(line + ":" + inetAddress.getHostAddress());
+				}
+			} catch(UnknownHostException ex) {
+				System.out.println("unknown host");
+			}
 			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-
+		
+		scanner.close();
 	}
 
 }
