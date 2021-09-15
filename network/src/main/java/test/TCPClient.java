3930d0eeab24d70b4981d9e998829b97e43a6a6c
douzone-bipa <douzone.bipa@gmail.com>
2021-09-15 14:19:21 +0900

    TCP Client #1

--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -1,9 +1,38 @@
 package test;
 
-public class TCPClient {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
 
+public class TCPClient {
+	private static final String SERVER_IP = "127.0.0.1";
+	private static final int SERVER_PORT = 5000;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Socket socket = null;
+		
+		try {
+			//1. 소켓 생성
+			socket = new Socket();
+			
+			//2. 서버 연결
+			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+			
+			
+		} catch (SocketException e) {
+			System.out.println("[client] suddenly closed by server");
+		} catch (IOException e) {
+			System.out.println("[client] error: " + e);
+		} finally {
+			try {
+				if(socket != null && socket.isClosed() == false) {
+					socket.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
