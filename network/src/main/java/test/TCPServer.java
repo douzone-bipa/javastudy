6f013b35a056909b93d2b2f7ba54c2d987e4c531
douzone-bipa <douzone.bipa@gmail.com>
2021-09-15 14:08:40 +0900

    TCP Server test

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -15,7 +18,7 @@ public class TCPServer {
 			
 			//2. 바인딩(binding)
 			//   Socket에 InetSocketAddress(IPAddress + port)
-			//   IPAdresss : 0.0.0.0 : 모든 IP로 부터의 연결을 허용
+			//   IPAdresss : 0.0.0.0: 특정 호스트 IP바인딩 하지 않는다. 
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000));
 			
 			//3. accept
@@ -26,6 +29,38 @@ public class TCPServer {
 			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
 			int remoteHostPort = inetRemoteSocketAddress.getPort();
 			System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remoteHostPort + "]");
+			
+			try {
+				//4. IO Stream 받아오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				while(true) {
+					//5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer);   // blocking
+					if(readByteCount == -1) {
+						// 클라이언트가 정상적으로 종료(close() 호출)
+						System.out.println("[server] closed by client");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "utf-8");
+					System.out.println("[server] received:" + data);
+				}
+			} catch(SocketException e) {
+				System.out.println("[server] suddenly closed by client");
+			} catch(IOException e) {
+				System.out.println("[server] error" + e);
+			} finally {
+				try {
+					if(socket != null && socket.isClosed() == false) {
+						socket.close();
+					}
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
 			
 		} catch (IOException e) {
 			System.out.println("[server] error:" + e);
